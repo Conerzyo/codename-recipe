dee1a274001adc4fbed424074039002f2b81a611
Patrik Procházka <>
2020-07-04 20:19:02 +0200

    docker init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log("Hello my recipe app");
+  await app.listen(process.env.SERVER_PORT);
+  console.log("App listening on port:", process.env.SERVER_PORT);
 }
 bootstrap();
